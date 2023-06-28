ac113ad211965702ff80dd81d850cbe968cd1e76
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-06-28 15:38:33 +0700

    bài tập cắt giao diện

--- practiceCutTemplate/src/main/java/com/cg/controller/CustomerController.java
+++ practiceCutTemplate/src/main/java/com/cg/controller/CustomerController.java
@@ -1,2 +1,22 @@
-package com.cg.controller;public class CustomerController {
+package com.cg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    @GetMapping
+    public String showDashboardPage(){
+        return "customer/template";
+    }
+    @GetMapping("/list")
+    public String showListPage(){
+        return "customer/list";
+    }
+    @GetMapping("/create")
+    public String showCreatePage(){
+        return "customer/create";
+    }
 }
